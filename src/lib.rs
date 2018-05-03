4a866751083ab6114dcecef299026467f255b8f1
Stefan Kroboth <stefan.kroboth@gmail.com>
2018-05-03 15:48:44 +0200

    first try

--- src/lib.rs
+++ src/lib.rs
@@ -7,10 +7,98 @@
 
 //! Point Cloud Density Estimation
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+use std::cell::RefCell;
+use std::rc::Rc;
+
+type NodeRef = Rc<RefCell<Node>>;
+type Extent = (f64, f64);
+
+pub enum Node {
+    Bin {
+        pos: Vec<f64>,
+        val: f64,
+    },
+    Child {
+        bins: Vec<NodeRef>,
+        extent: Vec<Extent>,
+    },
+}
+
+impl Node {
+    pub fn new_bin(pos: Vec<f64>) -> Self {
+        Node::Bin { pos, val: 0.0 }
+    }
+
+    pub fn add_to_bin(&mut self, val: f64) -> &mut Self {
+        match *self {
+            Node::Bin {
+                pos: _,
+                val: ref mut v,
+            } => *v += val,
+            _ => panic!("Can only add values to bin."),
+        };
+        self
+    }
+
+    pub fn pos(&self) -> Vec<f64> {
+        match *self {
+            Node::Bin { pos: ref p, .. } => p.clone(),
+            _ => panic!("only applicable to bins"),
+        }
+    }
+
+    pub fn new_child() -> Self {
+        Node::Child {
+            bins: vec![],
+            extent: vec![],
+        }
+    }
+
+    pub fn add_bin(&mut self, bin: &NodeRef, extent: Vec<Extent>) -> &mut Self {
+        match *bin.borrow() {
+            Node::Bin { .. } => {
+                match *self {
+                    Node::Child {
+                        bins: ref mut b,
+                        extent: ref mut ext,
+                    } => {
+                        let bin = bin.clone();
+                        assert_eq!(bin.borrow().len(), extent.len());
+                        if b.len() == 0 {
+                            *ext = extent
+                                .iter()
+                                .zip((*bin.borrow()).pos().iter())
+                                .map(|((a, b), c)| (a + c, b + c))
+                                .collect();
+                        }
+                        b.push(bin);
+                    }
+                    _ => panic!("Bins can only be added to Child"),
+                };
+            }
+            _ => unimplemented!(),
+        }
+        self
+    }
+
+    pub fn len(&self) -> usize {
+        match *self {
+            Node::Bin { pos: ref p, .. } => p.len(),
+            Node::Child { bins: ref b, .. } => b.len(),
+        }
+    }
+
+    pub fn as_ref(self) -> NodeRef {
+        Rc::new(RefCell::new(self))
     }
 }
+
+// struct Node {
+
+// #[cfg(test)]
+// mod tests {
+//     #[test]
+//     fn it_works() {
+//         assert_eq!(2 + 2, 4);
+//     }
+// }
