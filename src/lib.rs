823a2df88ebf050769233def6f13c15569d0abe9
Stefan Kroboth <stefan.kroboth@gmail.com>
2018-05-05 23:36:57 +0200

    minor restructuring

--- src/lib.rs
+++ src/lib.rs
@@ -8,48 +8,21 @@
 //! Point Cloud Density Estimation
 
 mod pyramid;
-mod tree;
+// mod tree;
 
-use tree::Node;
+pub use pyramid::Pyramid;
+// use tree::Node;
 
-fn bin_positions(
-    lim_x: (f64, f64),
-    lim_y: (f64, f64),
-    n_bins: (usize, usize),
-    extent: (usize, usize),
-) -> Vec<(f64, f64)> {
-    let border_x = (
-        lim_x.0 + (extent.0 as f64) / 2.0,
-        lim_x.1 - (extent.0 as f64) / 2.0,
-    );
-    let border_y = (
-        lim_y.0 + (extent.1 as f64) / 2.0,
-        lim_y.1 - (extent.1 as f64) / 2.0,
-    );
-    let step_x = (border_x.1 - border_x.0) / ((n_bins.0 - 1) as f64);
-    let step_y = (border_y.1 - border_y.0) / ((n_bins.1 - 1) as f64);
-    let mut out = Vec::with_capacity(n_bins.0 * n_bins.1);
-    for xi in 0..n_bins.0 {
-        for yi in 0..n_bins.1 {
-            out.push((
-                border_x.0 + (xi as f64) * step_x,
-                border_y.0 + (yi as f64) * step_y,
-            ));
-        }
-    }
-    out
-}
-
-pub fn build_tree(
-    lim_x: (f64, f64),
-    lim_y: (f64, f64),
-    n_bins: (usize, usize),
-    extent: (usize, usize),
-) -> Node {
-    let bin_pos = bin_positions(lim_x, lim_y, n_bins, extent);
-    println!("{:?}", bin_pos);
-    unimplemented!()
-}
+// pub fn build_tree(
+//     lim_x: (f64, f64),
+//     lim_y: (f64, f64),
+//     n_bins: (usize, usize),
+//     extent: (usize, usize),
+// ) -> Node {
+//     let bin_pos = bin_positions(lim_x, lim_y, n_bins, extent);
+//     println!("{:?}", bin_pos);
+//     unimplemented!()
+// }
 
 // #[cfg(test)]
 // mod tests {
--- src/pyramid.rs
+++ src/pyramid.rs
@@ -43,6 +43,34 @@ pub struct Pyramid {
     extents: Vec<Extent>,
 }
 
+fn bin_positions(
+    lim_x: (f64, f64),
+    lim_y: (f64, f64),
+    n_bins: (usize, usize),
+    extent: (usize, usize),
+) -> Vec<(f64, f64)> {
+    let border_x = (
+        lim_x.0 + (extent.0 as f64) / 2.0,
+        lim_x.1 - (extent.0 as f64) / 2.0,
+    );
+    let border_y = (
+        lim_y.0 + (extent.1 as f64) / 2.0,
+        lim_y.1 - (extent.1 as f64) / 2.0,
+    );
+    let step_x = (border_x.1 - border_x.0) / ((n_bins.0 - 1) as f64);
+    let step_y = (border_y.1 - border_y.0) / ((n_bins.1 - 1) as f64);
+    let mut out = Vec::with_capacity(n_bins.0 * n_bins.1);
+    for xi in 0..n_bins.0 {
+        for yi in 0..n_bins.1 {
+            out.push((
+                border_x.0 + (xi as f64) * step_x,
+                border_y.0 + (yi as f64) * step_y,
+            ));
+        }
+    }
+    out
+}
+
 impl Pyramid {
     pub fn new(
         (min_x, max_x): (f64, f64),
@@ -51,6 +79,9 @@ impl Pyramid {
         (extent_x, extent_y): (u64, u64),
     ) -> Self {
         // todo: n_bins_x and n_bins_y must be a power of 2
+        // first, create individual layers with their corresponding bin positions and extents
+        // second, connect the layers properly
+        let root = Node::new(vec![(min_x + max_x) / 2.0, (min_y + max_y) / 2.0]);
         unimplemented!()
     }
 }
