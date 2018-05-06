d40cb2247b7966e100db1faba85e6516013e2184
Stefan Kroboth <stefan.kroboth@gmail.com>
2018-05-06 22:18:27 +0200

    Semiautomatic update of documentation

---
+++ search-index.js
@@ -0,0 +1,3 @@
+var searchIndex = {};
+searchIndex["pcde"] = {"doc":"Point Cloud Density Estimation","items":[[3,"Pyramid","pcde","",null,null],[11,"new","","",0,null],[11,"push_extent","","",0,{"inputs":[{"name":"self"},{"generics":["f64"],"name":"vec"}],"output":{"name":"self"}}],[11,"push_node","","",0,{"inputs":[{"name":"self"},{"name":"rc"},{"name":"u64"}],"output":{"name":"self"}}]],"paths":[[3,"Pyramid"]]};
+initSearch(searchIndex);
