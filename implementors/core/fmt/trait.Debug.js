5755f343eb3e1dc60e48c66ed43d80a1c776a6b0
Stefan Kroboth <stefan.kroboth@gmail.com>
2018-05-27 21:07:46 +0200

    Semiautomatic update of documentation

---
+++ implementors/core/fmt/trait.Debug.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["pcde"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"struct\" href=\"pcde/struct.Pyramid.html\" title=\"struct pcde::Pyramid\">Pyramid</a>",synthetic:false,types:["pcde::pyramid::Pyramid"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
--- implementors/core/marker/trait.Sync.js
+++ implementors/core/marker/trait.Sync.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["pcde"] = [{text:"impl !<a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Sync.html\" title=\"trait core::marker::Sync\">Sync</a> for <a class=\"struct\" href=\"pcde/struct.Pyramid.html\" title=\"struct pcde::Pyramid\">Pyramid</a>",synthetic:true,types:["pcde::pyramid::Pyramid"]},];
+implementors["pcde"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Sync.html\" title=\"trait core::marker::Sync\">Sync</a> for <a class=\"struct\" href=\"pcde/struct.Pyramid.html\" title=\"struct pcde::Pyramid\">Pyramid</a>",synthetic:true,types:["pcde::pyramid::Pyramid"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
